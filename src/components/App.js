d4672212960f130a7226ebfb24cfac50da434881
hengstchon <hengstchon@gmail.com>
2020-08-12 22:12:24 +0200

    Update: dark mode

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "@material-ui/core";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -11,6 +11,10 @@ import Nav from "./Nav";
 export default () => {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   const [darkMode, setDarkMode] = useState(prefersDarkMode);
+
+  useEffect(() => {
+    setDarkMode(prefersDarkMode);
+  }, [prefersDarkMode]);
 
   const lightTheme = createMuiTheme({
     palette: {
